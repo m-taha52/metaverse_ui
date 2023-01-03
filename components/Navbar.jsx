f1a96d4129324e4d2e3ce9ae50ac48ff6854e303
unknown <mohammedt243@gmail.com>
2023-01-03 12:02:12 +0530

    Navbar
    Initializing imports for Navbar and wrapping it with a motion nav container

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,13 @@
 'use client';
 
+import {motion} from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav>
+
+  </motion.nav>
 );
 
 export default Navbar;
