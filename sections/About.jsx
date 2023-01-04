6248c43a12377591862b1132ba7717df9f8d849a
unknown <mohammedt243@gmail.com>
2023-01-05 03:54:33 +0530

    About
    Initialized the About section with a gradient div and a motion div
    proceeding to create a Typing Text Component which will be rendered in the About Component

--- sections/About.jsx
+++ sections/About.jsx
@@ -6,12 +6,19 @@ import { TypingText } from "../components";
 import styles from "../styles";
 import { fadeIn, staggerContainer} from "../utils/motion";
 
-
-
-
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0"> </div>
+    <motion.div
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView='show'
+    viewport={{once: false, amount:0.25}}
+    className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      
+    </motion.div>
+  About
   </section>
 );
 
