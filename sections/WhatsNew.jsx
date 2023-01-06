7eccf835e9d653d858777efa7655d8cc1dee21b7
unknown <mohammedt243@gmail.com>
2023-01-07 01:53:50 +0530

    Whats New
    Fixed a Typo

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import  styles  from '../styles';
 import { staggerContainer , fadeIn, planetVariants } from "../utils/motion";
-import { newFeatures, TitleText, TypingText} from "../components";
+import { NewFeatures, TitleText, TypingText} from "../components";
 import { newFeatures } from "../constants"
 
 const WhatsNew = () => (
