102c2d0088d593417764ff7248169fba71afceaa
unknown <mohammedt243@gmail.com>
2023-01-06 10:53:24 +0530

    Page
     Added Gradients to specific sections

--- app/page.js
+++ app/page.js
@@ -5,14 +5,29 @@ const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
+
+    <div className='relative '>
+      <About />
+      <div className='gradient-03 z-0'> </div>
+      <Explore />
+    </div>
+
+    <div className='relative'>
+     <GetStarted />
+     <div className='gradient-04 z-0'> </div>
+     <WhatsNew />
+    </div>
+
     <World />
+
+    <div> 
     <Insights />
+    <div className='gradient-04 z-0'> </div>
     <Feedback />
+    </div>
+
     <Footer />
+    
   </div>
 );
 
