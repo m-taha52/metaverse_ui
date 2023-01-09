d4f284c7d261a7867dc591caa0b50d2e801548b5
unknown <mohammedt243@gmail.com>
2023-01-09 09:19:44 +0530

    Insights
     Completed basic layout of the section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -7,9 +7,32 @@ import { InsightCard, TitleText, TypingText} from "../components";
 import { insights } from "../constants"
 
 const Insights = () => (
-  <section>
-    Insights section
-  </section>
+  <section className={`${styles.paddings} relative z-10 `}>
+      <motion.div
+      variants={staggerContainer}
+      viewport={{once: false, amount: 0.25}}
+      whileInView="show"
+      initial='hidden'
+      className={`${styles.innerWidth} mx-auto flex flex-col `}
+      >
+
+        <TypingText title="| Insights" textStyles="text-center" />
+        <TitleText title='Insight about metaverse' textStyles="text-center"  />
+
+        <div className="mt-[50px] flex flex-col gap-[30px]">
+          {insights.map((insight, index) => (
+            <InsightCard
+            key={`insight-${index}`}
+            {...insight}
+            index={index + 1}
+            />
+           
+            ))}
+        </div>
+
+
+      </motion.div>
+</section>
 );
 
 export default Insights;
