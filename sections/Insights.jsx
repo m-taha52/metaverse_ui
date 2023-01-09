f464cb8fc4b4248b55dd42648d3001b1ec73983c
unknown <mohammedt243@gmail.com>
2023-01-09 09:07:32 +0530

    Insights
    Initialized Imports

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from "framer-motion";
+import  styles  from '../styles';
+import { staggerContainer , fadeIn, planetVariants } from "../utils/motion";
+import { InsightCard, TitleText, TypingText} from "../components";
+import { insights } from "../constants"
+
 const Insights = () => (
   <section>
     Insights section
