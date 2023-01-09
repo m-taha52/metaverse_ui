ce65372360089894abce0bfc84dd208da3b63e91
unknown <mohammedt243@gmail.com>
2023-01-09 09:50:49 +0530

    Feedback
    Initialized Imports and Parent Container

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,22 @@
 'use client';
 
+import {motion} from "framer-motion"
+import styles from "../styles";
+import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
+
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    
+    <motion.div
+    variants={staggerContainer}
+    whileInView="show"
+    initial="hidden"
+    viewport={{once: 'false', amount: 0.25}}
+    className={`${styles.innerWidth} mx-auto flex flex-col `}
+    >
+
+    </motion.div>
   </section>
 );
 
